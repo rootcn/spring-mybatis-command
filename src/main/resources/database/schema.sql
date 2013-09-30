152f00d7c0e79d4cb8ae342884386d5061a45fd4
berry <berry@192.168.1.100>
2013-09-30 20:15:47 +0800

    generator config file

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -4,3 +4,22 @@ name  varchar(100) NULL ,
 PRIMARY KEY (keyId)
 )
 ;
+
+CREATE TABLE world (
+keyId  int NOT NULL ,
+name  varchar(100) NULL ,
+birthday  date NULL ,
+width  integer NULL ,
+can_arrive  integer NULL ,
+PRIMARY KEY (keyId)
+)
+;
+
+CREATE TABLE music (
+keyId  int NOT NULL ,
+name  varchar(100) NULL ,
+author  varchar(100) NULL ,
+pub_date  date NULL ,
+PRIMARY KEY (keyId)
+)
+;
---
+++ src/main/resources/generator/readme.txt
@@ -0,0 +1,4 @@
+
+command
+
+spring-mybatis-command>java -jar E:\dev\java\library\mybatis-generator-core-1.3.2\lib\mybatis-generator-core-1.3.2.jar -configfile src\main\resources\generator\generatorConfig.xml -overwrite
